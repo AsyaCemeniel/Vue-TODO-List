4a16a14e2d02677bd663677a27f8caaf1a8c0bc7
AsyaCemeniel <asyaorlov89@gmail.com>
2021-11-06 14:35:48 +0200

    Add styles to input

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <input type="text" class="todo-input" autofocus />
+    <button class="todo-button" type="submit">
+      <i class="fas fa-plus-circle"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 70%;
+}
+.todo-input {
+  background: #eefffa;
+  width: 85%;
+  padding: 8px 15px;
+  border-radius: 4px 0 0 4px;
+}
+.todo-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 15%;
+  background: #eefffa;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  border-radius: 0 4px 4px 0;
+  margin-right: 5px;
+}
+
+.todo-input,
+.todo-button {
+  outline: none;
+  font-size: 30px;
+  border: none;
+  color: #356cb6;
+}
+
+.todo-button:hover {
+  background: #356cb6;
+  color: #eefffa;
+}
+</style>
